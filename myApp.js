55dcb72d938785967a95257c89f708c2c9461e69
Tai Nalewyko <tai.nalewajko@pm.me>
2024-10-15 23:21:27 +0000

    try again...

--- myApp.js
+++ myApp.js
@@ -39,12 +39,21 @@ app.use("/public", express.static(__dirname + "/public"));
 //});
 
 /** 6) Use the .env file to configure the app */
-const message = "HELLO JSON"
-app.get("/json",
-    (req, res) => res.json(
-        {"message": process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message
-    })
-);
+//const message = "HELLO JSON"
+//app.get("/json",
+//    (req, res) => res.json(
+//        {"message": process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message
+//    })
+//);
+require('dotenv').config();
+
+app.get("/json", (req, res) => {
+  const message = "Hello json"; // Default message in lowercase
+  const responseMessage = process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message;
+  res.json({ "message": responseMessage });
+});
+
+
 
 
 /** 7) Root-level Middleware - A logger */
