f59406ba30c048171737cddd0dedbe409750c17e
Tai Nalewyko <tai.nalewajko@pm.me>
2024-10-15 23:16:27 +0000

    again...

--- myApp.js
+++ myApp.js
@@ -42,9 +42,7 @@ app.use("/public", express.static(__dirname + "/public"));
 const message = "HELLO JSON"
 app.get("/json",
     (req, res) => res.json(
-        {"message":
-        process.env.MESSAGE_STYLE === 
-        "uppercase" ? message.toUpperCase() : message
+        {"message": process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message
     })
 );
 
